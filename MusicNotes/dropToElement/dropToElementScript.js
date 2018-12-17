603f161cee64b2f70009650e6e31cb1c6dc5fa32
JulieStuneck <jbstuneck@yahoo.com>
2018-12-17 11:49:29 -0500

    changes

--- MusicNotes/dropToElement/dropToElementScript.js
+++ MusicNotes/dropToElement/dropToElementScript.js
@@ -1,13 +1,23 @@
-function allowDrop(ev) {
+/*function allowDrop(ev) {
   ev.preventDefault();
-}
+}*/
 
-function drag(ev) {
+allowDrop = ev => ev.preventDefault();
+
+/*function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
-}
+}*/
 
-function drop(ev) {
+drag = ev => ev.dataTransfer.setData("text", ev.target.id);
+
+/*function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}*/
+
+drop = ev => {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
 }
